b84b29e1f9612072e039d2ebf761485f8d86be53
Ed Jones <ed@errorstudio.co.uk>
2016-01-12 13:11:00 +0000

    Instance has meta attributes

--- lib/rooftop/events/models/instance.rb
+++ lib/rooftop/events/models/instance.rb
@@ -2,6 +2,7 @@ module Rooftop
   module Events
     class Instance
       include Rooftop::Post
+      include Rooftop::Events::MetaAttributes
       self.api_namespace = "rooftop-events"
       self.api_version = 2
       self.api_endpoint = "events/:event_id/instances"
