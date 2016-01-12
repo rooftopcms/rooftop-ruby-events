6c94824e5c0b758bf003e820aa064c46bb39b699
Ed Jones <ed@errorstudio.co.uk>
2016-01-12 13:10:33 +0000

    Event has MetaAttributes

--- lib/rooftop/events/models/event.rb
+++ lib/rooftop/events/models/event.rb
@@ -2,7 +2,7 @@ module Rooftop
   module Events
     class Event
       include Rooftop::Post
-      include Rooftop::Events::CustomAttributes
+      include Rooftop::Events::MetaAttributes
       self.api_namespace = "rooftop-events"
       self.api_version = 2
       has_many :instances, class: "Rooftop::Events::Instance"
