92dd2e2d9af761d1d5708665a4ceea9862bc3f6c
Ed Jones <ed@errorstudio.co.uk>
2016-01-12 13:10:14 +0000

    munge meta attributes before saving - wip

--- lib/rooftop/meta_attributes.rb
+++ lib/rooftop/meta_attributes.rb
@@ -1,6 +1,6 @@
 module Rooftop
   module Events
-    module CustomAttributes
+    module MetaAttributes
       def self.included(base)
         meta_method = case base.to_s.demodulize.underscore
                         when 'event'
@@ -9,14 +9,18 @@ module Rooftop
                           :"event_#{base.to_s.demodulize.underscore}_meta"
                       end
         base.send(:after_find, ->(record) {
+
           if record.respond_to?(meta_method)
-            record.custom_attributes = record.send(meta_method)[:custom_attributes]
+            record.meta_attributes = record.send(meta_method) || {}
+          else
+            record.meta_attributes = {}
           end
+
         })
 
         # before saving, we need to move stuff from custom_attributes back to meta_[event|event_instance]
         base.send(:before_save, ->(record) {
-          record.restore_custom_attributes!
+          record.send(:"#{meta_method}=", record.meta_attributes)
         })
       end
 
