6880d73cb5ee829bce1c0f123c4f0e95a2f5fcc7
Ed Jones <ed@errorstudio.co.uk>
2016-01-14 17:55:03 +0000

    wip -

--- lib/rooftop/events/models/price.rb
+++ lib/rooftop/events/models/price.rb
@@ -5,7 +5,14 @@ module Rooftop
       include Rooftop::Events::MetaAttributes
       self.api_namespace = "rooftop-events"
       self.api_version = 2
-      self.api_endpoint = "price_lists/:price_list_id/prices"
+      # self.api_endpoint = "price_lists/:price_list_id/prices"
+
+      def initialize(args)
+        super
+        if _embedded
+          @_price_list_id = _embedded[:price_list].first[:id]
+        end
+      end
 
       def price_band
         # TODO - we should be able to call resolve() on these links
@@ -16,6 +23,11 @@ module Rooftop
         # TODO - we should be able to call resolve() on these links
         TicketType.find(_embedded[:ticket_type].first[:id])
       end
+
+      # def _price_list_id
+      #   _embedded[:price_list].first[:id]
+      # end
+
 
     end
   end
