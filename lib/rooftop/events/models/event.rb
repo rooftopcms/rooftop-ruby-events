038449f7313ba5f4f0bfaf4c15b594dd1aed7909
Ed Jones <ed@errorstudio.co.uk>
2016-01-21 16:06:47 +0000

    Take options for related events

--- lib/rooftop/events/models/event.rb
+++ lib/rooftop/events/models/event.rb
@@ -7,8 +7,13 @@ module Rooftop
       self.api_version = 2
       has_many :instances, class: "Rooftop::Events::Instance"
 
-      def related_events
-        Event.get("#{self.class.collection_path}/#{self.id}/related_events")
+      def related_events(opts = {})
+        default_opts = {
+          per_page: 10
+        }
+        opts[:per_page] = opts[:count]
+        opts = default_opts.merge(opts)
+        Event.get("#{self.class.collection_path}/#{self.id}/related_events?#{opts.to_query}")
       end
     end
   end
